35485dbdfb5cf86a2001c6138976229c62f46eb3
cesar reyes <pacblack@outlook.com>
2017-09-04 08:47:35 -0600

    Se agregaron nuevos metodos a la hora de la realizaciòn del programa para asi hacerlo completamente funcional

--- Estacionamiento.java
+++ Estacionamiento.java
@@ -1,4 +1,27 @@
+
 public class Estacionamiento
 {
- 
+    int max=150, costo=15, vehiest=0, total=0;
+    String Arr[];
+    boolean Registrar(String placa,int Horaentrada,int Minutosentrada)
+    {
+        if(vehiest<=max)
+        {
+            Arr[vehiest]=placa;
+            vehiest+=1;
+            return(true);
+        }
+        else
+        {
+            System. out. println(" No hay espacio");
+            return(false);
+        } 
+        
+    
+    }
+    
+    boolean Salida(int Horasalida, int Minutossalida, String placa)
+    {
+    
+    }
 }
