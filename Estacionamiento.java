6c538997044dbcf89e759688c78da468b57a7176
cesar reyes <pacblack@outlook.com>
2017-09-04 08:57:19 -0600

    Se agrego todos los detalles faltantes, solo falta calcular el total a pagar

--- Estacionamiento.java
+++ Estacionamiento.java
@@ -1,13 +1,15 @@
 
 public class Estacionamiento
 {
-    int max=150, costo=15, vehiest=0, total=0;
+    int max=150, costo=15, vehiest=0, total, i,horaentradas[],minutosentrada[];
     String Arr[];
     boolean Registrar(String placa,int Horaentrada,int Minutosentrada)
     {
         if(vehiest<=max)
         {
             Arr[vehiest]=placa;
+            horaentradas[vehiest]=Horaentrada;
+            minutosentrada[vehiest]=Minutosentrada;
             vehiest+=1;
             return(true);
         }
@@ -22,6 +24,19 @@ public class Estacionamiento
     
     boolean Salida(int Horasalida, int Minutossalida, String placa)
     {
-    
+        for(i=0;i<vehiest;i+=1)
+        {
+            if(Arr[i] == placa)
+            {
+                Arr[i]=null;
+                return true;
+            }
+            else 
+            {
+                System. out. println(" No se encuentra el vehiculo en el sistema");   
+                return false;
+            }
+        }
+        return false;
     }
 }
